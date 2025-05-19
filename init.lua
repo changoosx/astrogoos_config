3c03788588a9283796701021432141e4583561a2
Changoos <97424400+changoosx@users.noreply.github.com>
2025-05-20 05:23:54 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -7,13 +7,51 @@ if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- validate that lazy is available
 if not pcall(require, "lazy") then
-  -- stylua: ignore
   vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
   vim.fn.getchar()
   vim.cmd.quit()
 end
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.cmd [[
+      hi Normal guibg=NONE ctermbg=NONE
+      hi NormalNC guibg=NONE ctermbg=NONE
+      hi NormalFloat guibg=NONE ctermbg=NONE
+      hi Pmenu guibg=NONE ctermbg=NONE
+    ]]
+  end,
+})
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.cmd [[
+      hi CursorLine guibg=#222222 guifg=NONE ctermbg=NONE ctermfg=NONE
+    ]]
+  end,
+})
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.cmd [[
+      hi StatusLine guibg=NONE ctermbg=NONE
+      hi StatusLineNC guibg=NONE ctermbg=NONE
+      hi WinBar guibg=NONE ctermbg=NONE
+      hi WinBarNC guibg=NONE ctermbg=NONE
+      hi TabLine guibg=NONE ctermbg=NONE
+      hi TabLineFill guibg=NONE ctermbg=NONE
+      hi TabLineSel guibg=NONE ctermbg=NONE
+    ]]
+  end,
+})
+
+vim.opt.laststatus = 0
+vim.opt.showtabline = 0
+vim.opt.winbar = ""
 
 require "lazy_setup"
 require "polish"
